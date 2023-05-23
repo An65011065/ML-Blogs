25fe15bc2a97ed00ee2a98121cc3938e56870778
An Adhikari <anweshana@middlebury.edu>
2023-05-23 05:48:32 -0400

    logisticREg

--- posts/Gradient Descent/LogisticRegression.py
+++ posts/Gradient Descent/LogisticRegression.py
@@ -21,36 +21,41 @@ class LogisticRegression:
             self.w -= alpha * gradient
 
             loss = (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
-            self.loss_history.append(loss)
+            self.loss_history=self.loss_history.append(loss)
 
             score = self.score(X, y)
+
             self.score_history=self.score_history.append(score)
+            
 
     def fit_stochastic(self, X, y, alpha=0.1, max_epochs=1000, batch_size=32):
         n, m = X.shape
         self.w = np.zeros(m+1)
         X_bias = np.insert(X, 0, 1, axis=1)
+        self.loss_history = []
+        self.score_history = []
         
         for epoch in range(max_epochs):
             order = np.random.permutation(n)
             X_shuffled = X_bias[order]
             y_shuffled = y[order]
             
-            for i in range(0, n, batch_size):
-                xi = X_shuffled[i:i+batch_size]
-                yi = y_shuffled[i:i+batch_size]
+            for sub in range(0, n, batch_size):
+
+                xsub = X_shuffled[sub:sub+batch_size]
+                ysub = y_shuffled[sub:sub+batch_size]
                 
-                z = np.dot(xi, self.w)
+                z = np.dot(xsub, self.w)
                 h = self.sigmoid(z)
-                gradient = np.dot(xi.T, (h - yi)) / batch_size
+                gradient = np.dot(xsub.T, (h - ysub)) / batch_size
                 self.w -= alpha * gradient
                 
-                loss = (-yi * np.log(h) - (1 - yi) * np.log(1 - h)).mean()
+                loss = (-ysub * np.log(h) - (1 - ysub) * np.log(1 - h)).mean()
                 self.loss_history.append(loss)
 
                 score = self.score(X, y)
                 self.score_history.append(score)
-            
+
     def predict(self, X):
         X_bias = np.insert(X, 0, 1, axis=1)
         return self.sigmoid(np.dot(X_bias, self.w)).round()
@@ -62,3 +67,4 @@ class LogisticRegression:
         X_bias = np.insert(X, 0, 1, axis=1)
         h = self.sigmoid(np.dot(X_bias, self.w))
         return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+##
